5965c8a43d1f813dee54f5543dca77cbdc2ab293
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2021-03-11 10:58:06 +0700

    add color pallete

---
+++ lib/constant/color_pallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:veluv/extension/color_extension.dart' as color;
+abstract class ColorPallete{
+  
+  static final Color primaryColor=color.ColorExtension.fromHex('#e44d6e');
+  static final Color primaryLightColor=color.ColorExtension.fromHex('#ff658b');
+  static final Color primaryTextColor= color.ColorExtension.fromHex('#212121');
+  static final Color backgroundColor=color.ColorExtension.fromHex('##fefefe');
+  static final Color backgroundCardColor=color.ColorExtension.fromHex('#ffffff');
+}
